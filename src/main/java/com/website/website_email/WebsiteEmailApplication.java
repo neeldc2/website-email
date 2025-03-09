2813c376db16f52095b6ee68f38d0f17c607010a
neeld <neeldc2@gmail.com>
2025-03-09 12:52:08 +0530

    Initial commit

---
+++ src/main/java/com/website/website_email/WebsiteEmailApplication.java
@@ -0,0 +1,13 @@
+package com.website.website_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/website_email/WebsiteEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.website_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
